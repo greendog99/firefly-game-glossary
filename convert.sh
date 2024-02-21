63b9a3f250adbebfdff278c9a6a39c85aae6dcc9
Mike Fischer <mikefischer99@gmail.com>
2024-02-21 15:33:24 -0500

    Final v1.1 files

--- convert.sh
+++ convert.sh
@@ -4,5 +4,5 @@ export GS=/opt/homebrew/bin/gs
 themes=(letter a4)
 
 for theme in "${themes[@]}"; do
-	asciidoctor-pdf -v -a optimize -a pdf-fontsdir="fonts;GEM_FONTS_DIR" --theme theme-$theme.yml firefly-glossary.adoc -o firefly-glossary-$theme.pdf
+  asciidoctor-pdf -v -a optimize -a pdf-fontsdir="fonts;GEM_FONTS_DIR" --theme theme-$theme.yml firefly-glossary.adoc -o firefly-glossary-$theme.pdf
 done
