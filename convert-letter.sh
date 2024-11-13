76b17094d7cbd313cd9133573d050c2a735f9f97
Mike Fischer <mikefischer99@gmail.com>
2024-11-13 14:10:47 -0500

    Added Alliance Alert Card update per BGG discussion.

--- convert-letter.sh
+++ convert-letter.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 export GS=/opt/homebrew/bin/gs
-themes=(letter a4)
+themes=(letter)
 
 for theme in "${themes[@]}"; do
   asciidoctor-pdf -v -a optimize -a pdf-fontsdir="fonts;GEM_FONTS_DIR" --theme theme-$theme.yml firefly-glossary.adoc -o firefly-glossary-$theme.pdf
-  #asciidoctor-pdf -v -a pdf-fontsdir="fonts;GEM_FONTS_DIR" --theme theme-$theme.yml firefly-glossary.adoc -o firefly-glossary-$theme.pdf
 done
